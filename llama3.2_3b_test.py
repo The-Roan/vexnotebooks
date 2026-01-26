958d18eae05eeda12d3eb1d2061ce20f6b49daf2
poinkpolkadots <olkadotspinkp@gmail.com>
2026-01-25 20:36:54 -0500

    added test file for both 1b and 3b versions of llama3.2

---
+++ llama3.2_3b_test.py
@@ -0,0 +1,19 @@
+import ollama
+import time
+
+start_time = time.time()
+
+response = ollama.chat(
+    model="llama3.2:3b",
+    messages=[
+        {"role": "user", "content": "5 questions for vex robotics judge to ask about engineering notebooks"}
+    ],
+    stream=True
+)
+
+for chunk in response:
+    print(chunk["message"]["content"], end="", flush=True)
+
+print("\n")
+
+print("time: %s second" % (time.time() - start_time))
